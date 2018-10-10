23fc43461442b7a63e937a16814b6183cc939055
SML <dltkdals2202@gmail.com>
2018-10-10 14:15:31 +0900

    1003 피보나치 함수

---
+++ DP/1003.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int mem[41];
+
+int fibonacci(int n) {
+    if (mem[n] > 0) return mem[n];
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return mem[n] = fibonacci(n - 2) + fibonacci(n - 1);
+}
+
+
+int main() {
+    int T, N;
+    scanf("%d", &T);
+
+    for (int i = 0; i < T; ++i) {
+        scanf("%d", &N);
+        if (N == 0) printf("1 0\n");
+        else printf("%d %d\n", fibonacci(N - 1), fibonacci(N));
+    }
+
+    return 0;
+}
